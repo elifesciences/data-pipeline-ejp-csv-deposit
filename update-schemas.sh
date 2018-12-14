d8a8083af72b6d43fab74a97c3c3f9d53d4be5b8
Luke <lsh-0@users.noreply.github.com>
2018-12-14 15:21:12 +1030

    adds new table to schema dump dump

--- update-schemas.sh
+++ update-schemas.sh
@@ -14,5 +14,8 @@ function dump {
 dump 380_datascience_early_career_researchers
 dump 455_datascience_editors
 dump 489_datascience_editor_keywords
+# reports 705 and 728 both go to the same table
 dump 705_datascience_reviewer_identity_revealed_last_week
 dump 767_datascience_person_roles
+# reports 779 and 780 both go to the same table
+dump 779_datascience_reviewer_info_all
