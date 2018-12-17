c5ef42943f7e4bd94006418c0d059089b4da502a
Luke <lsh-0@users.noreply.github.com>
2018-12-17 13:33:09 +1030

    adds 'provenance.source_fiename' to json output if filename passed as argument

--- scripts/tests/test_ejpcsv2json.py
+++ scripts/tests/test_ejpcsv2json.py
@@ -21,3 +21,16 @@ def test_standard_parse():
         {'poa_m_ms_id': '38546', 'poa_m_ms_no': '27706', 'poa_r_received_dt': '2017-04-11 15:38:47.637', 'poa_r_receipt_dt2': '2017-06-01 03:31:21.817', 'date_generated': '2018-11-18', 'imported_timestamp': '2018-11-01T23:59:59Z'},
         {'poa_m_ms_id': '42025', 'poa_m_ms_no': '30325', 'poa_r_received_dt': '2017-07-11 08:08:57.670', 'poa_r_receipt_dt2': '2017-07-26 05:14:54.213', 'date_generated': '2018-11-18', 'imported_timestamp': '2018-11-01T23:59:59Z'}]
     assert expected == actual
+
+@pytest.mark.freeze_time(time_frozen_at)
+def test_standard_parse_with_filename():
+    buffer = io.StringIO()
+    writer = lambda x: buffer.write(str(x) + "\n")
+    filename = "ejp_query_tool_query_id_705_DataScience:_Reviewer_identity_revealed_-_last_week_2018_10_19_eLife.csv"
+    ecj.main(io.StringIO(simplest_fixture), writer, filename=filename)
+    json_lines = buffer.getvalue().splitlines()
+    actual = list(map(json.loads, json_lines))
+    expected = [
+        {'poa_m_ms_id': '38546', 'poa_m_ms_no': '27706', 'poa_r_received_dt': '2017-04-11 15:38:47.637', 'poa_r_receipt_dt2': '2017-06-01 03:31:21.817', 'date_generated': '2018-11-18', 'imported_timestamp': '2018-11-01T23:59:59Z', 'provenance.source_filename': filename},
+        {'poa_m_ms_id': '42025', 'poa_m_ms_no': '30325', 'poa_r_received_dt': '2017-07-11 08:08:57.670', 'poa_r_receipt_dt2': '2017-07-26 05:14:54.213', 'date_generated': '2018-11-18', 'imported_timestamp': '2018-11-01T23:59:59Z', 'provenance.source_filename': filename}]
+    assert expected == actual
