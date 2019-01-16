82ba20d74c4818f46fcc40d0fcedd6e81e0e2b61
Luke <lsh-0@users.noreply.github.com>
2019-01-16 14:40:37 +1030

    removed --autodetect from load-bq script

--- scripts/load-file-to-bq.sh
+++ scripts/load-file-to-bq.sh
@@ -9,6 +9,5 @@ path_to_source="$4"
 bq --location="$location" load \
     --headless \
     --source_format=NEWLINE_DELIMITED_JSON \
-    --autodetect \
     "$dataset.$table" \
     "$path_to_source"
