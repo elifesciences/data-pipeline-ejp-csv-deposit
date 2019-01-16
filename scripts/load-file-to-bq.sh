df2b7d0551c98ac39c08add244d4cd2954e3494d
Luke <lsh-0@users.noreply.github.com>
2019-01-16 13:42:48 +1030

    adds load-file-to-bq.sh script

---
+++ scripts/load-file-to-bq.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -eu
+
+location="$1"
+dataset="$2"
+table="$3"
+path_to_source="$4"
+
+bq --location="$location" load \
+    --source_format=NEWLINE_DELIMITED_JSON \
+    --autodetect \
+    "$dataset.$table" \
+    "$path_to_source"
